114e8f98d9c8f362e6a5f3a518ba7bf47b21d71c
Tom Gantzer <tomgantzer@gmail.com>
2016-06-09 09:42:46 +0100

    Added support for orientation via accelerometer, removed redundant node modules, added development modules, enabled JSON reading/writing via node file system, continued development on vote system, created helper functions for javascript range

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 // SERVER VARIABLES
-var EventEmitter = require('events');
-var http = require("http");
+  // file system and networks
+var fs = require('fs');
+var os = require('os');
+
+  // server + dependencies (express)
+var http = require('http');
 var express = require('express');
+var expressHBS = require('express-handlebars');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 var server = http.createServer(app);
+
+  // client websockets and REST
 var socketio = require('socket.io')(server);
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var unirest = require('unirest');
-var five = require("johnny-five");
+
+  // arduino interfacing (johnny5)
+var five = require('johnny-five');
+var Particle = require('particle-io');
 var LifxClient = require('node-lifx').Client;
 var light = new LifxClient();
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var Particle = require("particle-io");
+
+  // database storage
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
 // BOARD SETUP
 var arduinos = [
@@ -26,20 +38,31 @@ var arduinos = [
     id: "pressure",
     port: "/dev/cu.usbmodem1421"
   },
+  // {
+  //   id: "particle",
+  //   io: new Particle({
+  //     token: "1059f47f854fca3cd19ccb2b050f8f31d0b227e8",
+  //     deviceId: "38001b000447353138383138"
+  //   })
+  // },
   {
-    id: "particle",
+    id: "particle2",
     io: new Particle({
       token: "1059f47f854fca3cd19ccb2b050f8f31d0b227e8",
-      deviceId: "38001b000447353138383138"
+      deviceId: "36003a000a47353235303037"
     })
   }
 ];
 var boards = new five.Boards(arduinos);
 
 // CONFIGURATION
+var jsonData = {};
+var jsonDataBackup = {};
 var port = 8080;
 var brightness = 0;
 var delay = 120;
+
+var tiltThreshold = 0.15;
 
 var aylaEmail = "tom.gantzer@thealloy.com";
 var aylaPass = "Chicago1TG";
@@ -60,18 +83,50 @@ var accessToken = "";
 var refreshToken = "";
 
 // SERVER SETUP
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + '/'));
-app.get('/app/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'));
+app.use('/json', express.static(__dirname + '/'));
+
+// set handlebars as the view templating language
+app.engine('.hbs', expressHBS({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', __dirname + '/views');
+
+  // routing for each page
+  // home page showing brightness control
+app.get('/', function(req, res) {
+  res.render('index', {title: "Alloy SHW 2016", stylesheet: "main"});
+});
+  // vote page
+app.get('/vote', function(req, res) {
+  res.render('index', {title: "Alloy SHW 2016 Vote", stylesheet: "vote", layout: "vote"});
+});
+  // results page
+app.get('/results', function(req, res) {
+  res.render('index', {title: "Alloy SHW 2016 Results", stylesheet: "results", layout: "results"});
 });
 
-// WORKING POST REQUEST TO GET ACCESS TOKEN FOR AYLA API
+// INITIALISATION
+server.listen(port, function() {
+  console.log('Server available at ' + 'http://localhost:' + port);
+});
 
+fs.readFile('votes.json', 'utf8', function(err, data) {
+  if (err) {
+    console.log("Error retrieving votes json", err);
+    return;
+  }
+  else {
+    jsonData = JSON.parse(data);
+    console.log("jsonData loaded with data from votes.json");
+  }
+});
+
+//WORKING POST REQUEST TO GET ACCESS TOKEN FOR AYLA API
 // unirest.post(aylaURL)
 // .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
 // .send(aylaData)
@@ -83,9 +138,6 @@ app.get('/app/', function(req, res) {
 //
 //   //unirest.get("" + "?auth_token=" + accessToken);
 // });
-
-server.listen(port);
-console.log('Server available at ' + 'http://localhost:' + port);
 
 // LETS DO THIS!
 boards.on("ready", function() {
@@ -105,42 +157,17 @@ boards.on("ready", function() {
   //   freq: delay
   // });
 
-  var tilt1 = new five.Sensor({
+  var imu = new five.IMU({
     board: this.byId("particle"),
-    type: "digital",
-    pin: "D0"
-  });
-
-  var tilt2 = new five.Sensor({
-    board: this.byId("particle"),
-    type: "digital",
-    pin: "D1"
-  });
-
-  var tilt3 = new five.Sensor({
-    board: this.byId("particle"),
-    type: "digital",
-    pin: "D2"
-  });
-
-  var tilt4 = new five.Sensor({
-    board: this.byId("particle"),
-    type: "digital",
-    pin: "D3"
+    controller: "MPU6050",
+    device: "GY-521",
+    freq: 500
   });
 
   var mat = new five.Button({
     board: this.byId("pressure"),
     pin: 2,
     isPullup: true
-  });
-
-  var accel = new five.Accelerometer({
-    board: this.byId("particle"),
-    pins: ["A0", "A1", "A2"],
-    sensitivity: 128,
-    // zeroV: 478,
-    zeroV: 330
   });
 
   var motion = new five.Motion({
@@ -150,7 +177,6 @@ boards.on("ready", function() {
   });
 
   // SENSOR LOGIC
-
   light.on('light-new', function(bulb) {
 
     bulb.getState(function(error, data){
@@ -158,16 +184,16 @@ boards.on("ready", function() {
       console.log(data);
     });
 
-    proximity.on("data", function() {
-      if (this.cm < 100) {
-        brightness = Math.min(Math.max(parseInt(this.cm), 0), 100);
-        brightness = 100 - brightness;
-        console.log("Range:  " + brightness);
-        bulb.color(0, 100, brightness, 3500, delay, function() {
-          socketio.in('clients').emit("ui update", brightness);
-        });
-      }
-    });
+    // proximity.on("data", function() {
+    //   if (this.cm < 100) {
+    //     brightness = Math.min(Math.max(parseInt(this.cm), 0), 100);
+    //     brightness = 100 - brightness;
+    //     console.log("Range:  " + brightness);
+    //     bulb.color(0, 100, brightness, 3500, delay, function() {
+    //       socketio.in('clients').emit("ui update", brightness);
+    //     });
+    //   }
+    // });
 
     mat.on("down", function(value) {
       bulb.getState( function(error, data) {
@@ -184,31 +210,95 @@ boards.on("ready", function() {
       });
     });
 
-    motion.on("calibrated", function() {
-      console.log("calibrated");
-    });
+    // motion.on("calibrated", function() {
+    //   console.log("calibrated");
+    // });
+    //
+    // motion.on("motionstart", function() {
+    //   console.log("motion start");
+    //   bulb.on();
+    // });
+    //
+    // motion.on("motionend", function() {
+    //   console.log("motion end");
+    //   bulb.off();
+    // });
 
-    motion.on("motionstart", function() {
-      console.log("motion start");
-      bulb.on();
-    });
+    // tilt1.on("change", function() {
+    //   console.log("1 TILTED!!!!");
+    // });
+    // tilt2.on("change", function() {
+    //   console.log("2 TILTED!!!!");
+    // });
+    // tilt3.on("change", function() {
+    //   console.log("3 TILTED!!!!");
+    // });
+    // tilt4.on("change", function() {
+    //   console.log("4 TILTED!!!!");
+    // });
 
-    motion.on("motionend", function() {
-      console.log("motion end");
-      bulb.off();
-    });
+    imu.on("data", function() {
+      // LOGIC TABLE
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "top") {
+          console.log("top");
 
-    tilt1.on("change", function() {
-      console.log("1 TILTED!!!!");
-    });
-    tilt2.on("change", function() {
-      console.log("2 TILTED!!!!");
-    });
-    tilt3.on("change", function() {
-      console.log("3 TILTED!!!!");
-    });
-    tilt4.on("change", function() {
-      console.log("4 TILTED!!!!");
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 100, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "bottom") {
+          console.log("bottom");
+
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 0, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "front") {
+          console.log("front");
+
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 40, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "back") {
+          console.log("back");
+
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 80, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "left") {
+          console.log("left");
+
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 20, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
+        if (getCubeSide(this.accelerometer.x,this.accelerometer.y,this.accelerometer.z) == "right") {
+          console.log("right");
+
+          bulb.getState( function(error, data) {
+            if (data.power) {
+              bulb.color(0, 100, 60, 3500, delay);
+              console.log("brightness: " + data.color.brightness);
+            }
+          });
+        }
     });
 
     // accel.on("orientation", function(data) {
@@ -219,33 +309,10 @@ boards.on("ready", function() {
   light.init();
 });
 
-  // PARTICLE CONNECTIONS
-
-// particleBoard.on("ready", function() {
-//   console.log("particle connected.");
-//   // SENSOR SETUP
-//   var particleProx = new five.Proximity({
-//     controller: "HCSR04",
-//     pin: 7,
-//     freq: delay
-//   });
-//
-//   // SENSOR LOGIC
-//   particleProx.on("data", function() {
-//       // brightness = Math.min(Math.max(parseInt(this.cm), 0), 100);
-//       // brightness = 100 - brightness;
-//       console.log("Particle Range:  " + this.cm);
-//       // bulb.color(0, 100, brightness, 3500, delay, function() {
-//       //   socketio.in('clients').emit("ui update", brightness);
-//       // });
-//   });
-// });
-
   // SOCKET CONNECTIONS
+
 socketio.on('connection', function(socket){
   console.info('New client connected ('+ socket.id +').');
-
-  socket.join('clients');
 
   socket.on('brightness change', function(data){
     console.log(data);
@@ -255,4 +322,70 @@ socketio.on('connection', function(socket){
   socket.on('disconnect', function() {
     console.log(socket.id + ' disconnected');
   });
+
+    // when client sends their vote data, increment the database store, then send socket back including new vote tally for client results page.
+  socket.on("vote", function(voteData) {
+    console.log("vote received for " + voteData);
+    jsonData[voteData].votes++;
+    fs.writeFile('votes.json', JSON.stringify(jsonData, null, 2), function (err) {
+      if (err) {
+        console.log('There has been an error saving your configuration data.', err);
+        return;
+      }
+      else {
+        console.log("New vote tally: " + jsonData[voteData].votes);
+        socket.emit("vote tallied");
+        socketio.emit("new vote", jsonData);
+      }
+    });
+  });
 });
+
+// HELPER FUNCTIONS
+
+// function between(x, min, max) {
+//   return x >= min && x <= max;
+// }
+
+Number.prototype.between = function(a, b) {
+  var min = Math.min(a, b),
+    max = Math.max(a, b);
+
+  return this > min && this < max;
+};
+
+function getCubeSide(x,y,z) {
+  if (x.between(0-tiltThreshold, 0+tiltThreshold) &&
+      y.between(0-tiltThreshold, 0+tiltThreshold) &&
+      z.between(-1-tiltThreshold, -1+tiltThreshold)) {
+    return "top";
+  }
+  if (x.between(0-tiltThreshold, 0+tiltThreshold) &&
+      y.between(0-tiltThreshold, 0+tiltThreshold) &&
+      z.between(1-tiltThreshold, 1+tiltThreshold)) {
+    return "bottom";
+  }
+  if (x.between(0-tiltThreshold, 0+tiltThreshold) &&
+      y.between(1-tiltThreshold, 1+tiltThreshold) &&
+      z.between(0-tiltThreshold, 0+tiltThreshold)) {
+    return "front";
+  }
+  if (x.between(0-tiltThreshold, 0+tiltThreshold) &&
+      y.between(-1-tiltThreshold, -1+tiltThreshold) &&
+      z.between(0-tiltThreshold, 0+tiltThreshold)) {
+    return "back";
+  }
+  if (x.between(-1-tiltThreshold, -1+tiltThreshold) &&
+      y.between(0-tiltThreshold, 0+tiltThreshold) &&
+      z.between(0-tiltThreshold, 0+tiltThreshold)) {
+    return "left";
+  }
+  if (x.between(1-tiltThreshold, 1+tiltThreshold) &&
+      y.between(0-tiltThreshold, 0+tiltThreshold) &&
+      z.between(0-tiltThreshold, 0+tiltThreshold)) {
+    return "right";
+  }
+  else {
+    return false;
+  };
+}
---
+++ public/js/main.js
@@ -0,0 +1,117 @@
+var socket = io();
+var jsonData = {};
+var ctx;
+
+$(document).ready(function() {
+	// JQUERY LOADED
+	if (top.location.pathname === '/results') {
+
+		$.getJSON("/json/votes.json").done(function(data) {
+      jsonData = data;
+			console.log(jsonData);
+
+			// CHART CONFIGURATION
+			console.log("im working every reload.");
+			ctx = $("#resultsChart");
+			var resultsChart = new Chart(ctx, {
+				type: 'horizontalBar',
+				data: {
+						labels: ["Hand", "Cube", "Mobile App"],
+						datasets: [{
+								label: 0,
+								data: [jsonData.hand.votes, jsonData.cube.votes, jsonData.app.votes],
+								backgroundColor: [
+										'rgba(255, 99, 132, 0.2)',
+										'rgba(54, 162, 235, 0.2)',
+										'rgba(255, 206, 86, 0.2)'
+								],
+								borderColor: [
+										'rgba(255,99,132,1)',
+										'rgba(54, 162, 235, 1)',
+										'rgba(255, 206, 86, 1)'
+								],
+								borderWidth: 1
+						}]
+				},
+				options: {
+						scales: {
+								yAxes: [{
+										ticks: {
+												beginAtZero:true
+										}
+								}]
+						}
+				}
+			});
+    });
+	}
+
+	$('#slider').rangeslider({
+		polyfill: false,
+		rangeClass: "rangeslider",
+    disabledClass: "rangeslider--disabled",
+    horizontalClass: "rangeslider--horizontal",
+    verticalClass: "rangeslider--vertical",
+    fillClass: "rangeslider__fill",
+    handleClass: "rangeslider__handle",
+		onInit: function() {
+			console.log("rangeslider initialised");
+		},
+		onSlideEnd: function(position, value) {
+			console.log("rangeslider ended...", value);
+			socket.emit("brightness change", value);
+		}
+	});
+
+	$('#button-up').click(function() {
+		var value =  parseInt($('#slider').val());
+		if (value >= 0 && value <= 100) {
+			$('#slider').val(value + 10).change();
+			value =  parseInt($('#slider').val());
+			console.log(value);
+			socket.emit("brightness change", value);
+		}
+	});
+
+	$('#button-down').click(function() {
+		var value =  parseInt($('#slider').val());
+		if (value >= 0 && value <= 100) {
+			$('#slider').val(value - 10).change();
+			value =  parseInt($('#slider').val());
+			console.log(value);
+			socket.emit("brightness change", value);
+		}
+	});
+
+	$('#button-done').click(function() {
+		window.location.href = "/vote";
+	});
+
+	$('#button-hand').click(function() {
+		// $("#button-vote span").html("for Hand");
+		socket.emit("vote", "hand");
+	});
+	$('#button-cube').click(function() {
+		// $("#button-vote span").html("for Cube");
+		socket.emit("vote", "cube");
+	});
+	$('#button-app').click(function() {
+		// $("#button-vote span").html("for Mobile App");
+		socket.emit("vote", "app");
+	});
+});
+
+socket.on("ui update", function(data) {
+	console.log("received ui update socket - ", data);
+	$('#slider').val(data).change();
+});
+
+socket.on("vote tallied", function(data) {
+	window.location.href = "/results";
+});
+
+socket.on("new vote", function(voteData) {
+	if (top.location.pathname === '/results') {
+		location.reload();
+	}
+});
